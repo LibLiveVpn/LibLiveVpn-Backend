13b967865181b33e3029183583ced4e28b601e99
libsyfer <libsyfer@gmail.com>
2024-12-12 13:36:41 +0300

    Создание класса области vpn

---
+++ LibLiveVpn-Backend.Domain/Models/VpnNetworkArea.cs
@@ -0,0 +1,15 @@
+﻿namespace LibLiveVpn_Backend.Domain.Models
+{
+    public class VpnNetworkArea
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string BaseIpAddress { get; set; } = string.Empty;
+
+        public int Members { get; set; }
+
+        public int MembersLimit { get; set; }
+    }
+}
