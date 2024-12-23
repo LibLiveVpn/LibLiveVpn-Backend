483f0b9d7673a34186860f899eb030869b2da440
libsyfer <libsyfer@gmail.com>
2024-12-23 19:59:31 +0300

    Создание контроллеров управления пользователями и подключениями

---
+++ LibLiveVpn-Backend.API/Controllers/UsersController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibLiveVpn_Backend.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
