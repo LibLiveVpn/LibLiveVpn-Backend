859dfafa0699423b1d92b7d38cf61d8c2004c225
libsyfer <libsyfer@gmail.com>
2024-12-23 20:00:31 +0300

    Добавление контроллера для просмотра и поиска доступных серверов

---
+++ LibLiveVpn-Backend.API/Controllers/ServersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibLiveVpn_Backend.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServersController : ControllerBase
+    {
+    }
+}
