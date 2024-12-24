987e8ab4220f6d6f8df4fbadf0931dfa72b79815
libsyfer <libsyfer@gmail.com>
2024-12-24 14:40:16 +0300

    Создание моделей для создания и обновления пользователей

---
+++ LibLiveVpn-Backend.API/Models/CreateUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LibLiveVpn_Backend.API.Models
+{
+    public class CreateUserDto
+    {
+        public string Login { get; set; } = null!;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ LibLiveVpn-Backend.API/Models/UpdateUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LibLiveVpn_Backend.API.Models
+{
+    public class UpdateUserDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
