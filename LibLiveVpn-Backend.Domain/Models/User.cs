4fbd785843cdcf7157bcac6c3c3347994d42ad1e
libsyfer <libsyfer@gmail.com>
2024-12-24 14:37:15 +0300

    Удаление полей сущности пользователя

--- LibLiveVpn-Backend.Domain/Models/User.cs
+++ LibLiveVpn-Backend.Domain/Models/User.cs
@@ -4,9 +4,11 @@
     {
         public Guid Id { get; set; }
 
-        public string Login { get; set; } = string.Empty;
+        public string Login { get; set; } = null!;
 
-        public string PasswordHash { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
 
         public List<VpnConfigurationSummary> Configurations { get; set; } = new List<VpnConfigurationSummary>();
     }
