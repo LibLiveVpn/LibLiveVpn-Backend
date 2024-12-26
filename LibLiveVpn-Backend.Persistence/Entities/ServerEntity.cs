7a7522b533956522277bbdb76d931591ae50a6b9
libsyfer <libsyfer@gmail.com>
2024-12-26 14:11:06 +0300

    Создание классов сущности пользователей и серверов базы данных

---
+++ LibLiveVpn-Backend.Persistence/Entities/ServerEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace LibLiveVpn_Backend.Persistence.Entities
+{
+    public class ServerEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Ip { get; set; } = null!;
+
+        public int Port { get; set; } = -1;
+
+        public string Provider { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ LibLiveVpn-Backend.Persistence/Entities/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace LibLiveVpn_Backend.Persistence.Entities
+{
+    public class UserEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Login { get; set; } = null!;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
