93e6213b2f01766895eaede9a632d086df97f776
libsyfer <libsyfer@gmail.com>
2024-12-26 15:06:53 +0300

    Реализация репозитория серверов

--- LibLiveVpn-Backend.Persistence/Repositories/ServerRepositoryEfCore.cs
+++ LibLiveVpn-Backend.Persistence/Repositories/ServerRepositoryEfCore.cs
@@ -1,5 +1,7 @@
 ﻿using LibLiveVpn_Backend.Application.Interfaces.Repositories;
 using LibLiveVpn_Backend.Domain.Models;
+using LibLiveVpn_Backend.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibLiveVpn_Backend.Persistence.Repositories
 {
@@ -12,29 +14,107 @@ namespace LibLiveVpn_Backend.Persistence.Repositories
             _context = context;
         }
 
-        public Task<IEnumerable<Server>> GetAsync(CancellationToken cancellationToken)
+        public async Task<IEnumerable<Server>> GetAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return await _context.Servers.AsNoTracking()
+                .Select(e => new Server
+                {
+                    Id = e.Id,
+                    Ip = e.Ip,
+                    Port = e.Port,
+                    Provider = e.Provider,
+                    Name = e.Name,
+                    Description = e.Description,
+                })
+                .ToListAsync(cancellationToken);
         }
 
-        public Task<Server?> GetByIdAsync(Guid serverId, CancellationToken cancellationToken)
+        public async Task<Server?> GetByIdAsync(Guid serverId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return await _context.Servers.AsNoTracking()
+                .Select(e => new Server
+                {
+                    Id = e.Id,
+                    Ip = e.Ip,
+                    Port = e.Port,
+                    Provider = e.Provider,
+                    Name = e.Name,
+                    Description = e.Description,
+                })
+                .FirstOrDefaultAsync(e => e.Id == serverId, cancellationToken);
         }
 
-        public Task<Server?> CreateAsync(Server server, CancellationToken cancellationToken)
+        public async Task<Server?> CreateAsync(Server server, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            server.Id = Guid.NewGuid();
+
+            var serverEntity = new ServerEntity
+            {
+                Id = server.Id,
+                Ip = server.Ip,
+                Port = server.Port,
+                Provider = server.Provider,
+                Name = server.Name,
+                Description = server.Description
+            };
+
+            await _context.Servers.AddAsync(serverEntity, cancellationToken);
+
+            try
+            {
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+            catch
+            {
+                return null;
+            }
+
+            return server;
         }
 
-        public Task<Server?> UpdateAsync(Server server, CancellationToken cancellationToken)
+        public async Task<Server?> UpdateAsync(Server server, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var existedServer = await _context.Servers.FindAsync(server.Id, cancellationToken);
+            if (existedServer == null)
+            {
+                return null;
+            }
+
+            existedServer.Name = server.Name;
+            existedServer.Description = server.Description;
+
+            try
+            {
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+            catch
+            {
+                return null;
+            }
+
+            return server;
         }
 
-        public bool Delete(Guid serverId)
+        public async Task<bool> DeleteAsync(Guid serverId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var existedServer = await _context.Servers.FindAsync(serverId, cancellationToken);
+            if (existedServer == null)
+            {
+                return false;
+            }
+
+            _context.Servers.Remove(existedServer);
+
+            try
+            {
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
