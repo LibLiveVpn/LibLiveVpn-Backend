d1544668ef3be65c74de36c71cbe77c3e1d23eae
libsyfer <libsyfer@gmail.com>
2024-12-26 16:22:05 +0300

    Создание модели конфигурации базы данных

---
+++ LibLiveVpn-Backend.Persistence/DbConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace LibLiveVpn_Backend.Persistence
+{
+    public class DbConfiguration
+    {
+        public const string Section = "DatabaseConnectionSettings";
+
+        public string Host { get; set; } = null!;
+
+        public int Port { get; set; }
+
+        public string Database { get; set; } = null!;
+
+        public string Username { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
