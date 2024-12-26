dc4af2f739166b442e9318c79ecfde54a49880c3
libsyfer <libsyfer@gmail.com>
2024-12-26 14:13:19 +0300

    Создание классов реализаций репозиториев

---
+++ LibLiveVpn-Backend.Persistence/Repositories/ServerRepositoryEfCore.cs
@@ -0,0 +1,40 @@
+﻿using LibLiveVpn_Backend.Application.Interfaces.Repositories;
+using LibLiveVpn_Backend.Domain.Models;
+
+namespace LibLiveVpn_Backend.Persistence.Repositories
+{
+    public class ServerRepositoryEfCore : IServerRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServerRepositoryEfCore(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IEnumerable<Server>> GetAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Server?> GetByIdAsync(Guid serverId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Server?> CreateAsync(Server server, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Server?> UpdateAsync(Server server, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid serverId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LibLiveVpn-Backend.Persistence/Repositories/UserRepositoryEfCore.cs
@@ -0,0 +1,40 @@
+﻿using LibLiveVpn_Backend.Application.Interfaces.Repositories;
+using LibLiveVpn_Backend.Domain.Models;
+
+namespace LibLiveVpn_Backend.Persistence.Repositories
+{
+    public class UserRepositoryEfCore : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepositoryEfCore(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IEnumerable<User>> GetAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetByIdAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> CreateAsync(User user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> UpdateAsync(User user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
