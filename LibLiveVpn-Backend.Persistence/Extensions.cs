ec7c8c999590f83ff87f217dd5eacbd1c29ce392
libsyfer <libsyfer@gmail.com>
2024-12-26 16:24:49 +0300

    Реализация функций метода расширения для подключения к базе данных

--- LibLiveVpn-Backend.Persistence/Extensions.cs
+++ LibLiveVpn-Backend.Persistence/Extensions.cs
@@ -1,14 +1,36 @@
 ﻿using LibLiveVpn_Backend.Application.Interfaces.Repositories;
 using LibLiveVpn_Backend.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LibLiveVpn_Backend.Persistence
 {
     public static class Extensions
     {
-        public static IServiceCollection AddPersistenceDependencies(this IServiceCollection services)
+        public static IServiceCollection AddPersistenceDependencies(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<ApplicationDbContext>();
+            var databaseConnectionSection = configuration.GetSection(DbConfiguration.Section);
+            var databaseConfiguration = new DbConfiguration();
+
+            databaseConfiguration.Host = databaseConnectionSection["Host"] ?? throw new ArgumentNullException($"{DbConfiguration.Section}:Host not exists");
+            if (!int.TryParse(databaseConnectionSection["Port"], out int port))
+            {
+                throw new ArgumentNullException("Database:Port not exists");
+            }
+            databaseConfiguration.Port = port;
+            databaseConfiguration.Database = databaseConnectionSection["Database"] ?? throw new ArgumentNullException($"{DbConfiguration.Section}:Database not exists");
+            databaseConfiguration.Username = databaseConnectionSection["Username"] ?? throw new ArgumentNullException($"{DbConfiguration.Section}:Username not exists");
+            databaseConfiguration.Password = databaseConnectionSection["Password"] ?? throw new ArgumentNullException($"{DbConfiguration.Section}:Password not exists");
+
+
+            services.AddDbContext<ApplicationDbContext>(opt =>
+            {
+                opt.UseNpgsql($"Host={databaseConfiguration.Host};Port={databaseConfiguration.Port};Database={databaseConfiguration.Database};Username={databaseConfiguration.Username};Password={databaseConfiguration.Password}", options =>
+                {
+                    options.EnableRetryOnFailure();
+                });
+            });
 
             services.AddScoped<IServerRepository, ServerRepositoryEfCore>();
             services.AddScoped<IUserRepository, UserRepositoryEfCore>();
