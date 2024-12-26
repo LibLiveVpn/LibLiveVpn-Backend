6bc9c76c5b93274429c7c7c3a8a5e023ebf70e80
libsyfer <libsyfer@gmail.com>
2024-12-26 16:35:23 +0300

    Добавление зависимости проекта API от проекта Persistence и подключение его зависимостей

--- LibLiveVpn-Backend.API/Program.cs
+++ LibLiveVpn-Backend.API/Program.cs
@@ -1,6 +1,9 @@
+using LibLiveVpn_Backend.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddPersistenceDependencies(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
