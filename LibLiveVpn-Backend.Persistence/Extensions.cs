2363e0d2035f65dd60a83d08436ddc66e7fddda7
libsyfer <libsyfer@gmail.com>
2024-12-26 14:13:45 +0300

    Создание метода расширения для настройки DI

---
+++ LibLiveVpn-Backend.Persistence/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using LibLiveVpn_Backend.Application.Interfaces.Repositories;
+using LibLiveVpn_Backend.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibLiveVpn_Backend.Persistence
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddPersistenceDependencies(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>();
+
+            services.AddScoped<IServerRepository, ServerRepositoryEfCore>();
+            services.AddScoped<IUserRepository, UserRepositoryEfCore>();
+
+            return services;
+        }
+    }
+}
