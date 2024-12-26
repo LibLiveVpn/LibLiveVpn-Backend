5f04d512cfe00c96f456b13c540bf56062640251
libsyfer <libsyfer@gmail.com>
2024-12-26 14:11:37 +0300

    Обновление модели контекста базы данных

--- LibLiveVpn-Backend.Persistence/ApplicationDbContext.cs
+++ LibLiveVpn-Backend.Persistence/ApplicationDbContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LibLiveVpn_Backend.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibLiveVpn_Backend.Persistence
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public DbSet<ServerEntity> Servers { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     }
 }
