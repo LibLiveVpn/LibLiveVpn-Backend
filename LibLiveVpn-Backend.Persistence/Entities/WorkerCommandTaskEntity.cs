255f50f47a5ddc3d328a78a1d0bc07e64b45227f
libsyfer <libsyfer@gmail.com>
2025-01-04 00:34:05 +0300

    Создание сущности выполняемой команды EF Core

---
+++ LibLiveVpn-Backend.Persistence/Entities/WorkerCommandTaskEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace LibLiveVpn_Backend.Persistence.Entities
+{
+    public class WorkerCommandTaskEntity
+    {
+        public Guid Id { get; set; }
+
+        public bool InProccess { get; set; } = true;
+
+        public string CommandName { get; set; } = null!;
+
+        public string InterfaceName { get; set; } = null!;
+
+        public int StatusCode { get; set; } = -1;
+
+        public string Detail { get; set; } = null!;
+    }
+}
