a8d53ea390e1e917fe37a024885461d295a2d556
libsyfer <libsyfer@gmail.com>
2025-01-03 16:20:17 +0300

    Создание класса конфигурации брокера сообщений

---
+++ LibLiveVpn-Backend.Infrastructure/Models/MessageBrokerConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace LibLiveVpn_Backend.Infrastructure.Models
+{
+    public class MessageBrokerConfiguration
+    {
+        public const string Section = "MessageBrokerSettings";
+
+        public string Host { get; set; } = null!;
+
+        public ushort Port { get; set; }
+
+        public string Username { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
