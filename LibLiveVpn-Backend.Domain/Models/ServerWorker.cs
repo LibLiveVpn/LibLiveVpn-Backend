a3b0debf5f8849f0d6ee7a3ca227b985251cba76
libsyfer <libsyfer@gmail.com>
2025-01-03 22:55:14 +0300

    Добавление базовой сущности воркера

---
+++ LibLiveVpn-Backend.Domain/Models/ServerWorker.cs
@@ -0,0 +1,13 @@
+﻿namespace LibLiveVpn_Backend.Domain.Models
+{
+    public class ServerWorker
+    {
+        public Guid Id { get; set; }
+
+        public string Ip { get; set; } = null!;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
