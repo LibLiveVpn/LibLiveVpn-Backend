e8cd56ab5317df3c193b945038c1db7a85d0efb8
libsyfer <libsyfer@gmail.com>
2025-01-04 00:33:39 +0300

    Создание базовой сущности выполняемой команды

---
+++ LibLiveVpn-Backend.Domain/Models/WorkerCommandTask.cs
@@ -0,0 +1,17 @@
+﻿namespace LibLiveVpn_Backend.Domain.Models
+{
+    public class WorkerCommandTask
+    {
+        public Guid Id { get; set; }
+
+        public bool InProccess { get; set; } = true;
+
+        public string CommandName { get; set; } = null!;
+
+        public string InterfaceName { get; set; } = null!;
+
+        public int StatusCode { get; set; } = -1;
+
+        public string Detail { get; set; } = null!;
+    }
+}
